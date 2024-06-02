0a4abf12e1fda6971abe0547b5d36e5abe09bcf3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 06:56:08 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const amount = useSelector((store) => store.basket.amount);
+
   return (
     <div
-      className="flex flex-row justify-evenly items-center
-     mt-2 pt-4 text-lg font-medium bg-gray-500"
+      className="flex flex-row justify-evenly 
+     mt-2 pt-4 text-lg font-medium bg-gray-400"
     >
       <p>Home</p>
       <p>
@@ -15,7 +17,7 @@ const Navbar = () => {
           className="text-xs align-top bg-red-500 text-white
          rounded-full px-2 py-1 mx-1"
         >
-          0
+          {amount}
         </span>
       </p>
     </div>
