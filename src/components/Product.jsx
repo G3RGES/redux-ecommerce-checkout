394c86022d5852e819bad67fe5d500c5ab9e70dd
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 07:48:11 +0300

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Product = ({ name, price, image, amount }) => {
-  return <div>Product</div>;
+  return (
+    <div className="flex flex-row items-center gap-8 px-10 border border-gray-200">
+      <img className="w-40" src={image} alt={name} />
+      <div className="flex flex-row items-center justify-between gap-3">
+        <div className="w-1/2 px-10">
+          <p className="text-xl font-medium">{name}</p>
+          <p className="text-lg tracking-wide">$ {price}</p>
+          <button
+            className="text-red-500 tracking-wide pt-1 pb-2
+        text-lg font-medium "
+          >
+            Remove
+          </button>
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-lg font-medium">Amount</p>
+          <div className="flex flex-row items-center gap-3 font-medium ">
+            <button className="text-red-700 text-xl">-</button>
+            <p>{amount}</p>
+            <button className="text-green-700 text-xl">+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
