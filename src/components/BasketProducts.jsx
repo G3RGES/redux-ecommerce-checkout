0d62091bbc1201ba3479fae3f3ad6b78e2e635ab
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 08:20:59 +0300

    Update BasketProducts.jsx

--- src/components/BasketProducts.jsx
+++ src/components/BasketProducts.jsx
@@ -3,27 +3,34 @@ import { useSelector } from "react-redux";
 import Product from "./Product";
 
 const BasketProducts = () => {
-  const { products, total } = useSelector((store) => store.basket);
+  const { products, total, amount } = useSelector((store) => store.basket);
 
   return (
     <div className="py-4">
-      <div>
-        {products.map((product, idx) => (
-          <Product
-            key={idx}
-            //   key={new Date().getTime()+Math.random()}//* DIFFERENT WAY TO HAVE A KEY
-            className=""
-            name={product.name}
-            price={product.price}
-            image={product.image}
-            amount={product.amount}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row items-center justify-evenly">
-        <p className="text-2xl font-medium">Total</p>
-        <p className="text-2xl font-medium">${total}</p>
-      </div>
+      {amount > 0 ? (
+        <>
+          {products.map((product, idx) => (
+            <Product
+              key={idx}
+              //   key={new Date().getTime()+Math.random()}//* DIFFERENT WAY TO HAVE A KEY
+              className=""
+              name={product.name}
+              price={product.price}
+              image={product.image}
+              amount={product.amount}
+            />
+          ))}
+
+          <div className="flex flex-row items-center justify-evenly">
+            <p className="text-2xl font-medium">Total</p>
+            <p className="text-2xl font-medium">${total}</p>
+          </div>
+        </>
+      ) : (
+        <p className="text-center text-2xl font-semibold text-red-800">
+          No Products
+        </p>
+      )}
     </div>
   );
 };
