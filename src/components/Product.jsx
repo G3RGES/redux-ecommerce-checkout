4a3bf35fb42ff722a885abb82fd18fce4d0af3e3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 08:13:46 +0300

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+import { useDispatch } from "react-redux";
+import {
+  increaseAmount,
+  decreaseAmount,
+  removeProduct,
+} from "../features/basketSlice";
+
 const Product = ({ name, price, image, amount }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="flex flex-row items-center gap-8 px-10 border border-gray-200">
       <img className="w-40" src={image} alt={name} />
@@ -11,6 +20,7 @@ const Product = ({ name, price, image, amount }) => {
           <button
             className="text-red-500 tracking-wide pt-1 pb-2
         text-lg font-medium "
+            onClick={() => dispatch(removeProduct({ name }))}
           >
             Remove
           </button>
@@ -18,9 +28,23 @@ const Product = ({ name, price, image, amount }) => {
         <div className="flex flex-col items-center">
           <p className="text-lg font-medium">Amount</p>
           <div className="flex flex-row items-center gap-3 font-medium ">
-            <button className="text-red-700 text-xl">-</button>
+            <button
+              className="text-red-700 text-xl"
+              onClick={() => {
+                amount > 1
+                  ? dispatch(decreaseAmount({ name }))
+                  : dispatch(removeProduct({ name }));
+              }}
+            >
+              -
+            </button>
             <p>{amount}</p>
-            <button className="text-green-700 text-xl">+</button>
+            <button
+              className="text-green-700 text-xl"
+              onClick={() => dispatch(increaseAmount({ name }))}
+            >
+              +
+            </button>
           </div>
         </div>
       </div>
