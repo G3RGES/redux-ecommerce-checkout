970ba99f351ec860d5015971aeef154fb49c52f3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 08:13:48 +0300

    Update basketSlice.js

--- src/features/basketSlice.js
+++ src/features/basketSlice.js
@@ -10,6 +10,35 @@ const initialState = {
 const basketSlice = createSlice({
   name: "basket",
   initialState,
+  reducers: {
+    increaseAmount: (state, { payload }) => {
+      const item = state.products.find((item) => item.name === payload.name);
+      item.amount++;
+    },
+    decreaseAmount: (state, { payload }) => {
+      const item = state.products.find((item) => item.name === payload.name);
+      item.amount--;
+    },
+    removeProduct: (state, { payload }) => {
+      state.products = state.products.filter(
+        (item) => item.name !== payload.name
+      );
+    },
+    updateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+
+      state.products.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
 
+export const { increaseAmount, decreaseAmount, removeProduct, updateTotal } =
+  basketSlice.actions;
 export default basketSlice.reducer;
