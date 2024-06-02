0f9f45f4c35bd8c26ae8fe8b750284ffede19732
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 07:21:21 +0300

    Update BasketProducts.jsx

--- src/components/BasketProducts.jsx
+++ src/components/BasketProducts.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Product from "./Product";
 
 const BasketProducts = () => {
-  return <div>BasketProducts</div>;
+  const { products, total, amount } = useSelector((store) => store.basket);
+
+  return (
+    <div>
+      {products.map((product, idx) => (
+        <Product
+          key={idx}
+          //   key={new Date().getTime()+Math.random()}//* DIFFERENT WAY TO HAVE A KEY
+          className=""
+          name={product.name}
+          price={product.price}
+          image={product.image}
+          amount={product.amount}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BasketProducts;
