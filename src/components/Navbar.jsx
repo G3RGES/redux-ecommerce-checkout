3f1a0aea46fedd2070e412c12dbb86cc18cb8245
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 06:48:55 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="flex flex-row justify-evenly items-center
+     mt-2 pt-4 text-lg font-medium bg-gray-500"
+    >
+      <p>Home</p>
+      <p>
+        Cart
+        <span
+          className="text-xs align-top bg-red-500 text-white
+         rounded-full px-2 py-1 mx-1"
+        >
+          0
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Navbar;
