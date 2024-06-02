a18595f89f9ffd492c9613c56b23974c9e37f308
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 07:48:09 +0300

    Update BasketProducts.jsx

--- src/components/BasketProducts.jsx
+++ src/components/BasketProducts.jsx
@@ -3,21 +3,27 @@ import { useSelector } from "react-redux";
 import Product from "./Product";
 
 const BasketProducts = () => {
-  const { products, total, amount } = useSelector((store) => store.basket);
+  const { products, total } = useSelector((store) => store.basket);
 
   return (
-    <div>
-      {products.map((product, idx) => (
-        <Product
-          key={idx}
-          //   key={new Date().getTime()+Math.random()}//* DIFFERENT WAY TO HAVE A KEY
-          className=""
-          name={product.name}
-          price={product.price}
-          image={product.image}
-          amount={product.amount}
-        />
-      ))}
+    <div className="py-4">
+      <div>
+        {products.map((product, idx) => (
+          <Product
+            key={idx}
+            //   key={new Date().getTime()+Math.random()}//* DIFFERENT WAY TO HAVE A KEY
+            className=""
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            amount={product.amount}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row items-center justify-evenly">
+        <p className="text-2xl font-medium">Total</p>
+        <p className="text-2xl font-medium">${total}</p>
+      </div>
     </div>
   );
 };
