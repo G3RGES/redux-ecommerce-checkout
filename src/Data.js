97395f6ef3ea50fe12ca92123a41fd45e8cbdd76
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 06:57:43 +0300

    Create Data.js

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const storeData = [
+  {
+    name: "Browline Glasses",
+    price: 100,
+    image: "https://i.ebayimg.com/images/g/BJwAAOSwAOxiK~dF/s-l1200.jpg",
+  },
+  {
+    name: "Cat-Eye Sunglasses",
+    price: 75,
+    image:
+      "https://eg.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/42/298962/1.jpg",
+  },
+  {
+    name: "Round Sunglasses",
+    price: 40,
+    image:
+      "https://eg.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/85/019744/1.jpg",
+  },
+  {
+    name: "Wayfarer Sunglasses",
+    price: 60,
+    image: "https://i.ebayimg.com/images/g/gw0AAOSwgX9clmYu/s-l1200.webp",
+  },
+  {
+    name: "Pilot Sunglasses",
+    price: 80,
+    image:
+      "https://ae01.alicdn.com/kf/A8a5e16b832d44b68b360ef2741234d9en/Aviation-Men-Polarized-Sunglasses-Frames-for-Men-Pilot-Sun-Glasses-Metal-Light-Flexible-Fashionable-Modern-Gold.jpg",
+  },
+];
