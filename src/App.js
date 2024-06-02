16c65209fbf2079c137598ff843714d6f3da18ef
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-02 07:08:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import BasketProducts from "./components/BasketProducts";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
+
       <h1 className="text-3xl font-semibold text-center mt-2 py-3">
         Redux E-Commerce Checkout
       </h1>
+
+      <BasketProducts />
     </>
   );
 }
